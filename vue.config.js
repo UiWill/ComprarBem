08c88ec901c6316a5145e823f1811664554a126e
Ui_WIll <williamemanoel777@gmail.com>
2025-07-08 00:09:23 -0300

    Adicionando build para GitHub Pages

--- vue.config.js
+++
@@ -1,9 +0,0 @@
-const { defineConfig } = require('@vue/cli-service')
-
-module.exports = defineConfig({
-  transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/ComprarBem/' // Nome correto do repositório
-    : '/',
-  lintOnSave: false // Desabilita o lint durante o build
-}) 
